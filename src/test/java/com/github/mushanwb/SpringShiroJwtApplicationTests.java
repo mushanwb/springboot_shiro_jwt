58e2f63fd2b13a1540cac25aaf170b4ed033646c
wangbo <1216476406@qq.com>
2020-11-23 18:17:50 +0800

    初始化框架，并且引入 shiro 和 jwt

---
+++ src/test/java/com/github/mushanwb/SpringShiroJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mushanwb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShiroJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
